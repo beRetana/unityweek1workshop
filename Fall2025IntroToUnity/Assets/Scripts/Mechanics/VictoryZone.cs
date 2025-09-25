ad4acc09140c14fc445903b221b7798b59b5cc8d
ctnp-e <catnipiscatweed@gmail.com>
2025-09-24 23:04:04 -0700

    made victory zone frustrating
    void OnTriggerEnter2D(Collider2D collider)
    {
        var p = collider.gameObject.GetComponent<PlayerController>();
        if (p != null)
        {
            var ev = Schedule<PlayerEnteredVictoryZone>();
            ev.victoryZone = this;
        }
    }

--- Fall2025IntroToUnity/Assets/Scripts/Mechanics/VictoryZone.cs
+++ Fall2025IntroToUnity/Assets/Scripts/Mechanics/VictoryZone.cs
@@ -9,14 +9,6 @@ namespace Platformer.Mechanics
     /// </summary>
     public class VictoryZone : MonoBehaviour
     {
-        void OnTriggerEnter2D(Collider2D collider)
-        {
-            var p = collider.gameObject.GetComponent<PlayerController>();
-            if (p != null)
-            {
-                var ev = Schedule<PlayerEnteredVictoryZone>();
-                ev.victoryZone = this;
-            }
-        }
+        
     }
 }
